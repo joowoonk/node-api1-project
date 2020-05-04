d740e95046cbfa367ead6b1ba3dc9fbf8afa4c4d
Golden Eagle <joowoon.kang@biola.edu>
2020-05-04 16:20:44 -0700

    post endpoint is working

--- index.js
+++ index.js
@@ -29,9 +29,23 @@ server.get("/api/users", (req, res) => {
 
 server.post("/api/users", function (req, res) {
   let userInformation = req.body;
-  users.push(userInformation);
-
-  res.status(201).json(users);
+  userInformation.id = shortid.generate();
+  console.log(userInformation.bio, userInformation.name);
+  if (
+    userInformation.bio === undefined ||
+    userInformation.name === undefined ||
+    userInformation.name === "" ||
+    userInformation.bio === ""
+  ) {
+    res.status(400).json({ Error: "Please insert name and bio" });
+  } else if (!userInformation) {
+    res.status(402).json({ Error: "There was an error tying to get the data" });
+  } else {
+    users.push(userInformation);
+    res.status(201).json(users);
+  }
 });
+
+//foreach for patch within in it else if. if user has id that matched, then person.name = user.name and person.bio = user.bio
 
 server.listen(8000, () => console.log("API is up boit!"));

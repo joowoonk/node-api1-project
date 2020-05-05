06183263674d6c813f6be6af2b1c9452796de58d
Golden Eagle <joowoon.kang@biola.edu>
2020-05-04 18:09:22 -0700

    added get by id

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ server.use(express.json());
 
 const shortid = require("shortid");
 
-let users = [
+const users = [
   {
     id: shortid.generate(),
     name: "Bruce Wayne",
@@ -44,6 +44,31 @@ server.post("/api/users", function (req, res) {
     users.push(userInformation);
     res.status(201).json(users);
   }
+});
+
+// When the client makes a GET request to /api/users/:id:
+
+// If the user with the specified id is not found:
+
+// respond with HTTP status code 404 (Not Found).
+// return the following JSON object: { message: "The user with the specified ID does not exist." }.
+// If there's an error in retrieving the user from the database:
+
+// respond with HTTP status code 500.
+// return the following JSON object: { errorMessage: "The user information could not be retrieved." }.
+
+server.get("/api/users/:id", (req, res) => {
+  const id = req.params.id;
+  let searchById = users.find((person) => person.id == id);
+  if (searchById) {
+    res.status(200).json(searchById);
+  } else if (!searchById) {
+    res.status(404).json({ Error: "The user by id doesn not exist" });
+  } else {
+    res.status(500).json({ Error: "The user info cannot be retrieved" });
+  }
+  //   console.log(searchById);
+  //   res.status(200).json(searchById);
 });
 
 //foreach for patch within in it else if. if user has id that matched, then person.name = user.name and person.bio = user.bio
